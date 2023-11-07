51b2970d7ee250cad33b001990a6de621f9701cb
Rene Lozano <rene.lozano@tecsup.edu.pe>
2023-11-07 09:17:46 -0500

    exercise events and inputs

--- js/chapter7Practice.js
+++ js/chapter7Practice.js
@@ -1,12 +1,30 @@
-console.log("practice 7")
+const nombre = document.getElementById("nombre")
+const correo = document.querySelector("#correo")
+const nombreMateria = document.querySelector("#nombreMateria")
 
-const nombre = document.querySelector("#nombre").value
-const correo = document.querySelector("#correo").value
-const nombreMateria = document.querySelector("#nombreMateria").value
+const spanNombre = document.querySelector("#validarNombre")
+const spanCorreo = document.querySelector("#validarCorreo")
+const spanNombreMateria = document.querySelector("#validarNombreMateria")
 
 const buttonSubmit = document.querySelector(".button-submit")
 
+// Expresiones regulares
+
 buttonSubmit.addEventListener("click", (e) => {
   e.preventDefault()
-  console.log(nombre, correo, nombreMateria)
+
+  let regexEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+
+  if (nombre.value.length < 5) {
+    spanNombre.innerHTML = "El nombre no es correcto"
+    spanNombre.className
+  }
+  if (!regexEmail.test(correo.value)) {
+    spanCorreo.innerHTML = "El correo no es correcto"
+  }
+  if (nombreMateria.value.length < 5) {
+    spanNombreMateria.innerHTML = "El nombre de la materia no es correcto"
+  }
+
+  // nombre.focus()
 })
